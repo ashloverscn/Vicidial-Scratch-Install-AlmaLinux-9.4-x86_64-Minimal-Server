b2991a50e85ff0b76f0a6d74c6af13d20064f66a
ashloverscn <ashloverscn@gmail.com>
2025-04-29 01:58:01 +0530

    Update part7.a.install-vicidial-crontab-from-file.sh

--- part7.a.install-vicidial-crontab-from-file.sh
+++ part7.a.install-vicidial-crontab-from-file.sh
@@ -5,6 +5,8 @@ sleep 2
 cd /usr/src
 \cp -r /usr/src/vici-cron /usr/src/vici-cron.original 
 #wget -O /usr/src/vici-cron https://github.com/ashloverscn/Vicidial-Scratch-Install-AlmaLinux-9.X-x86_64-Minimal-Server/raw/main/vici-cron
+#remove all enterieS in the crontab
+crontab -r
 #create a backup of the original crontab
 crontab -l > vici-cron.original
 #write crontab from a backup of the vicidial needed cronjobs
