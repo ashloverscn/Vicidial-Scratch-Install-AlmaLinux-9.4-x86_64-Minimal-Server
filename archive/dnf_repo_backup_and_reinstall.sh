88abd4af50446f7db358b4c12c2fe27728812528
ashloverscn <ashloverscn@gmail.com>
2025-04-29 05:41:25 +0530

    Add files via upload

--- archive/dnf_repo_backup_and_reinstall.sh
+++ archive/dnf_repo_backup_and_reinstall.sh
@@ -11,11 +11,17 @@ dnf makecache --refresh
 # Step 1: List minimal packages
 dnf groupinfo "Minimal Install" | awk '/Mandatory Packages:/,/^$/' | grep -v "Mandatory Packages:" | awk '{$1=$1};1' > minimal-packages.txt
 
-# Step 2: List all available packages
-dnf list available | awk '{print $1}' | grep -vE '^Available|^Last' > all-packages.txt
+# Step 2: List all available installable packages (binary only)
+dnf repoquery --available --qf "%{name}" > all-packages.txt
 
 # Step 3: Subtract minimal packages
 grep -vFxf minimal-packages.txt all-packages.txt > non-minimal-packages.txt
 
 # Step 4: Download non-minimal packages
-dnf download --resolve $(cat non-minimal-packages.txt)
+#dnf download --resolve $(cat non-minimal-packages.txt)
+#tell the script not to fail if one package is missing
+while read pkg; do
+    sudo dnf download --resolve "$pkg" || echo "Skipping $pkg"
+done < non-minimal-packages.txt
+
+chmod +x ./*.sh
