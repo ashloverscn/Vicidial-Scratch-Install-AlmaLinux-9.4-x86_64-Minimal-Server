30fa6caca695d22dbf9bd32bd75f6a923461a802
ashloverscn <ashloverscn@gmail.com>
2025-05-07 17:40:34 +0530

    Update part2.install-and-configure-php-mariadb-apache.sh

--- part2.install-and-configure-php-mariadb-apache.sh
+++ part2.install-and-configure-php-mariadb-apache.sh
@@ -121,7 +121,7 @@ sleep 2
 #read serveripadd
 # Retrieve the IP address
 serveripadd=$(hostname -I | awk '{print $1}')
-echo "IP Address\t: $serveripadd"
+echo "serveripadd is "$serveripadd
 
 sed -i "s/Redirect permanent \/ https:\/\/.*/Redirect permanent \/ https:\/\/$serveripadd\//g" /etc/httpd/conf.d/0000-default.conf
 
