b14dfc4bf84ace0357563514ddef278aa66700a0
ashloverscn <ashloverscn@gmail.com>
2025-10-02 21:14:11 +0530

    Update part2.install-and-configure-php-mariadb-apache.sh

--- part2.install-and-configure-php-mariadb-apache.sh
+++ part2.install-and-configure-php-mariadb-apache.sh
@@ -5,12 +5,12 @@ sleep 2
 
 dnf groupinstall "Development Tools" -y
 
-#dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
-#dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
-dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.0.noarch.rpm -y
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
+#dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.0.noarch.rpm -y
 
-#dnf install https://rpms.remirepo.net/enterprise/remi-release-9.rpm -y
-dnf install https://rpms.remirepo.net/enterprise/remi-release-10.0.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-9.rpm -y
+#dnf install https://rpms.remirepo.net/enterprise/remi-release-10.0.rpm -y
 
 dnf -y install yum-utils
 dnf module enable php:remi-7.4 -y
