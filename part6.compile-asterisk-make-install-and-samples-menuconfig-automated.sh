2b8fc766c01d05e2413ca9e35d9f1c1246960ab1
ashloverscn <ashloverscn@gmail.com>
2025-10-03 19:13:30 +0530

    Update part6.compile-asterisk-make-install-and-samples-menuconfig-automated.sh

--- part6.compile-asterisk-make-install-and-samples-menuconfig-automated.sh
+++ part6.compile-asterisk-make-install-and-samples-menuconfig-automated.sh
@@ -90,3 +90,20 @@ systemctl enable asterisk.service && \
 systemctl restart asterisk.service && \
 systemctl status asterisk.service | head -n 18
 
+cat <<ASTERISK>> /asterisk.sh
+
+#!/bin/bash
+
+service=$@asterisk
+/bin/systemctl -q is-active "$service.service"
+status=$?
+if [ "$status" == 0 ]; then
+    echo "$status"
+    echo "OK"
+else
+    /bin/systemctl restart "$service.service"
+fi
+
+ASTERISK
+
+chmod +x /asterisk.sh 
