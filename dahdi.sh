357392b528b8646f00602c93cd735748e1f56e3f
ashloverscn <ashloverscn@gmail.com>
2025-10-06 15:13:08 +0530

    Update dahdi.sh

--- dahdi.sh
+++ dahdi.sh
@@ -5,9 +5,13 @@ service=$@dahdi
 status=$?
 if [ "$status" == 0 ]; then
     echo "$status"
+    echo "DAHDI Service Already Running"
     echo "OK"
 else
     modprobe "$service"
     sleep 5
     /bin/systemctl restart "$service.service"
+    echo "DAHDI Service Started"
+    echo "OK"
+    
 fi
