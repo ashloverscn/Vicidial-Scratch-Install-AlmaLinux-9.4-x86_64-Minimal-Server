91ece69e2014a7bb9fb6197be3ce78cdf070e4f1
ashloverscn <ashloverscn@gmail.com>
2026-04-28 17:30:04 +0530

    Update part3.install-perl-cpan-cpanm-modules.sh

--- part3.install-perl-cpan-cpanm-modules.sh
+++ part3.install-perl-cpan-cpanm-modules.sh
@@ -25,8 +25,40 @@ yum install -y perl-CPAN perl-YAML perl-CPAN-DistnameInfo perl-libwww-perl perl-
 #curl -fsSL https://raw.githubusercontent.com/skaji/cpm/main/cpm | perl - install -g JSON::PP JSON::XS App::cpanminus App::cpm
 #/usr/local/bin/cpm install -g
 ##oneliner for CPM install using mirror
+#curl -fsSL https://raw.githubusercontent.com/skaji/cpm/main/cpm | perl - install -g --mirror http://www.cpan.org JSON::PP JSON::XS App::cpanminus App::cpm
+#/usr/local/bin/cpm install -g --mirror http://www.cpan.org 
+
+#!/bin/bash
+
+# --- 1. SETUP ISOLATED ENVIRONMENT ---
+# Create the directory for the modern Perl build
+mkdir -p /usr/local/perl5
+
+# --- 2. REBUILD PERL (THE ENGINE) ---
+# Download perl-build and compile Perl 5.38.0 into the isolated folder
+# This ensures we don't touch the system Perl (v5.16.3)
+curl -L https://raw.githubusercontent.com/tokuhirom/perl-build/master/perl-build | perl - 5.38.0 /usr/local/perl5
+
+# --- 3. ACTIVATE MODERN PERL ---
+# Point the current session to the new Perl binary
+export PATH=/usr/local/perl5/bin:$PATH
+
+# --- 4. BOOTSTRAP SKAJI MODULES ---
+# Use the modern Perl to install cpm and cpanminus
 curl -fsSL https://raw.githubusercontent.com/skaji/cpm/main/cpm | perl - install -g --mirror http://www.cpan.org JSON::PP JSON::XS App::cpanminus App::cpm
-/usr/local/bin/cpm install -g --mirror http://www.cpan.org 
+
+# --- 5. CREATE GLOBAL SYMLINKS ---
+# Map the new tools to /usr/local/bin so they are globally accessible
+ln -sf /usr/local/perl5/bin/cpm /usr/local/bin/cpm
+ln -sf /usr/local/perl5/bin/cpanm /usr/local/bin/cpanm
+
+# Refresh the shell's command path hash
+hash -r
+
+# --- 6. INSTALL TARGET MODULES ---
+# Finally, use the isolated cpm to install your required modules
+/usr/local/bin/cpm install -g --mirror http://www.cpan.org JSON::PP JSON::XS
+
 echo -e "\e[0;32m All cpan-modules installed and verified successfuly \e[0m"
 sleep 2
 
